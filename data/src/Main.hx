32b1cfab3aa5747868ada1b9909cfcecb9c5616a
Juan Delgado <juan@ustwo.co.uk>
2013-03-05 13:44:15 +0000

    Now getting video data from Youtube. Reading Youtube API key from properties file.

--- data/src/Main.hx
+++ data/src/Main.hx
@@ -1,5 +1,7 @@
 class Main
 {
+	var youtube_api_key : String = null;
+
 	var URLS_FILE_PATH : String = "template/urls.txt";
 
 	var LECTURES_TEMPLATE : String = "template/lectures.json";
@@ -8,19 +10,76 @@ class Main
 
 	var DATA_JSON_OUTPUT_PATH : String = "output/data.json";
 
+	var PROPERTIES_FILE : String = "properties/dev.properties";
+
 	function new()
 	{
+		if(!xa.File.isFile(PROPERTIES_FILE))
+		{
+			exit("Cannot find dev properties file");
+		}
+
+		var lines = xa.File.read(PROPERTIES_FILE).split("\n"); 
+
+		for(line in lines)
+		{
+			if(null == line || "" == line || StringTools.startsWith(line, "#"))
+			{
+				continue;
+			}
+
+			var bits = line.split("=");
+			var key = bits.shift();
+			var value = bits.join("=");
+
+			switch(key)
+			{
+				case "youtube_api_key":
+					youtube_api_key = value;
+			}
+		}
+
+		if(null == youtube_api_key)
+		{
+			exit("Cannot find Youtube API key in properties file");
+		}
+
+		log("Found Youtube API key");
+
 		var urls = xa.File.read(URLS_FILE_PATH).split("\n");
 
 		var lectures = [];
 
 		for (i in 0...urls.length)
 		{
+			var url = urls[i];
+			var id = url.substr(url.indexOf("=") + 1, url.length);
+
+			log("About to load video: " + url);
+
+			var atom_content = "";
+			var error = null;
+
+			var request = getRequest(id);
+			request.onData = function(data : String) : Void { atom_content = data; };
+			request.onError = function (msg : String) : Void { error = msg; };
+			request.request(false);
+
+			if(null != error)
+			{
+				log(error); 
+				continue;
+			}
+
+			var fast = new haxe.xml.Fast(Xml.parse(atom_content).firstElement());
+			var title = fast.node.title.innerHTML;
+			var description = fast.node.content.innerHTML;
+
 			lectures.push
 				({
 					id: i + 1,
-					title: "Title " + i,
-					description: "Description " + i,
+					title: haxe.Json.stringify(title),
+					description: haxe.Json.stringify(description),
 					url: urls[i]
 				});
 		}
@@ -35,9 +94,29 @@ class Main
 		xa.File.write(DATA_JSON_OUTPUT_PATH, data_template.execute({lectures: lectures_output.join("\n")}));
 	}
 
+	function getRequest(video_id : String) : haxe.Http
+	{
+		var url = "https://gdata.youtube.com/feeds/api/videos/" + video_id;
+
+		var request = new haxe.Http(url);
+		request.setHeader("X-GData-Key", "key=" + youtube_api_key);
+
+		return request;
+	}
+
 	function log(txt : String) : Void
 	{
 		xa.Utils.print(txt);
+	}
+
+	function exit(?txt) : Void
+	{
+		if(null != txt)
+		{
+			log(txt);
+		}
+
+		xa.Application.exit(1);
 	}
 
 	public static function main()
